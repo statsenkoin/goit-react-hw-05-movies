f982bdfb96c3b76c3fc80e74baab93cfe4e34d13
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-14 13:20:24 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import { Layout } from 'components';
+import { Home, Movies } from 'pages';
 import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<div>Home</div>} />
-        <Route path="movies" element={<div>Movies</div>} />
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
       </Route>
     </Routes>
   );

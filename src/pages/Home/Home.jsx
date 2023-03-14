ea608ab0d36a9ad47cc654d9d10bcdf2e6200bf8
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-14 21:35:33 +0200

    Feature/ add fetch, markup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,32 @@
-export const Home = () => {
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchTrending } from 'services/themoviedbApi';
+
+export default function Home() {
+  const [filmList, setFilmList] = useState([]);
+
+  useEffect(() => {
+    // console.log('filmList.length :>> ', filmList.length);
+    if (filmList.length > 0) return;
+    async function getTrendingFilms() {
+      try {
+        const trendingFilms = await fetchTrending();
+        setFilmList(trendingFilms);
+      } catch (error) {
+        console.log('error :>> ', error);
+      }
+    }
+    getTrendingFilms();
+  }, [filmList]);
   return (
-    // list of movies
-    <ul>
-      <li>Movie 01</li>
-    </ul>
+    <main>
+      <ol>
+        {filmList.map(({ title, id }) => (
+          <li key={id}>
+            <Link to={`movies/${id}`}>{title}</Link>
+          </li>
+        ))}
+      </ol>
+    </main>
   );
-};
+}

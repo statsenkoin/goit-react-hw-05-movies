534742fd40fa9a09d02028953ad88392286e637a
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-14 21:36:06 +0200

    Feature/ add base logic

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,3 +1,33 @@
-export const Movies = () => {
-  return <div>Movies Page</div>;
-};
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchMovieByName } from 'services/themoviedbApi';
+
+export default function Movies() {
+  const [filmList, setFilmList] = useState([]);
+
+  useEffect(() => {
+    if (filmList.length) return;
+    async function getFilmsByName() {
+      try {
+        const filmsByName = await fetchMovieByName('batman');
+        setFilmList(filmsByName);
+      } catch (error) {
+        console.log('error :>> ', error);
+      }
+    }
+    getFilmsByName();
+  }, [filmList]);
+  return (
+    <main>
+      <ul>
+        {filmList.map(({ title, id }) => (
+          <li key={id}>
+            <Link to={`${id}`}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+
+  // const trendingFilms = await fetchMovie('avatar');
+}

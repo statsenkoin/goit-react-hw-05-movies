f545158acee1c38834aaef0d66481a29b053a67e
statsenkoin <34393845+statsenkoin@users.noreply.github.com>
2023-03-14 19:37:03 +0000

    Deploying to gh-pages from @ statsenkoin/goit-react-hw-05-movies@534742fd40fa9a09d02028953ad88392286e637a 🚀

---
+++ static/js/186.56ea51e2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[186],{186:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(329);function u(){return(0,n.jsx)("div",{children:"Reviews subPage"})}}}]);
+//# sourceMappingURL=186.56ea51e2.chunk.js.map

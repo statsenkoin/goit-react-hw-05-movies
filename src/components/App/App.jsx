1f8f8635d2f490dc7989cc8fe4b75e3cca7f204f
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-14 21:34:24 +0200

    Feature/ add lazy import, add subRoutes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,14 @@
 import { Layout } from 'components';
-import { Home, Movies } from 'pages';
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
+const Home = lazy(() => import('../../pages/Home/Home'));
+const Movies = lazy(() => import('../../pages/Movies/Movies'));
+const MovieDetails = lazy(() =>
+  import('../../pages/MovieDetails/MovieDetails')
+);
+const Cast = lazy(() => import('../Cast/Cast'));
+const Reviews = lazy(() => import('../Reviews/Reviews'));
 
 export const App = () => {
   return (
@@ -8,6 +16,10 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
       </Route>
     </Routes>
   );

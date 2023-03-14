c4b68a8efef3fd56e6bd99b6196fb14199479ac0
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-14 21:33:43 +0200

    Feature/ add fetchMovieById + cleanup

--- src/services/themoviedbApi.js
+++ src/services/themoviedbApi.js
@@ -1,11 +1,29 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-axios.defaults.params = { key: 'e55542ecb6aab3d889d16953eac82937' };
-// https://api.themoviedb.org/3/trending/movie/day?api_key=e55542ecb6aab3d889d16953eac82937
+axios.defaults.params = {
+  api_key: 'e55542ecb6aab3d889d16953eac82937',
+};
+// https://api.themoviedb.org/3/trending/movie/day
+// https://api.themoviedb.org/3/search/movie?query=avatar
 // {
 // "page": 1,
 // "results": [20 items],
 // "total_pages": 1000,
 // "total_results": 20000
 // }
+
+export async function fetchTrending() {
+  const response = await axios.get(`/trending/movie/day`);
+  return response.data.results;
+}
+
+export async function fetchMovieByName(query = 'avatar') {
+  const response = await axios.get(`/search/movie?query=${query}`);
+  return response.data.results;
+}
+
+export async function fetchMovieById() {
+  const response = await axios.get(`/movie/{id}`);
+  return response.data.results;
+}

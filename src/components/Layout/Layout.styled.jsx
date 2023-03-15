88f64f0d3f7b41455c8d17b2824c82e6a1cc01bb
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-15 18:59:08 +0200

    Fix/ transition

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -37,6 +37,7 @@ export const NavLinkStyled = styled(NavLink)`
   }
   &:hover,
   &:focus {
+    transition: 300ms;
     scale: 1.1;
     box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
       0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);

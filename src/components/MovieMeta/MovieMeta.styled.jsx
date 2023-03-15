bf5d12abbe69fb73d2b02dd6b6337615388dfbef
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-16 00:36:42 +0200

    Create MovieMeta.styled.jsx

---
+++ src/components/MovieMeta/MovieMeta.styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  padding: 10px 0;
+  border-bottom: 2px solid #888;
+`;
+
+export const Image = styled.img`
+  width: 250px;
+`;
+
+export const Meta = styled.div`
+  padding: 20px;
+`;
+
+export const Title = styled.h3`
+  margin: 0;
+  font-size: 24px;
+  text-align: center;
+  text-decoration: underline;
+`;

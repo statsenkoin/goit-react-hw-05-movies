f26fd42a83664ab0fb8a0b4fcc0e9107d74f2c81
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-17 00:16:21 +0200

    Feature/ add search MovieByName, searchQuery

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,38 +1,36 @@
 import { useEffect, useState } from 'react';
 // import { Link, useLocation } from 'react-router-dom';
 import { fetchMovieByName } from 'services/themoviedbApi';
-import { PageWrapper, FilmList } from 'components';
+import { PageWrapper, FilmList, Searchbar } from 'components';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Movies() {
   const [filmList, setFilmList] = useState([]);
+  const [searchQuery, setSearchQuery] = useSearchParams();
   // const location = useLocation();
 
   useEffect(() => {
-    // if (filmList.length) return;
+    const query = searchQuery.get('query') ?? '';
     async function getFilmsByName() {
       try {
-        const filmsByName = await fetchMovieByName('batman');
+        const filmsByName = await fetchMovieByName(query);
         setFilmList(filmsByName);
       } catch (error) {
         console.log('error :>> ', error);
       }
     }
     getFilmsByName();
-  }, []);
+  }, [searchQuery]);
+
+  function handleSearchInput(query) {
+    const nextQuery = query !== '' ? { query } : {};
+    setSearchQuery(nextQuery);
+  }
 
   return (
     <PageWrapper>
+      <Searchbar handleSearchInput={handleSearchInput}></Searchbar>
       <FilmList filmList={filmList} />
-
-      {/* <div>
-        {filmList.map(({ title, id }) => (
-          <Link key={id} to={`${id}`} state={{ from: location }}>
-            {title}
-          </Link>
-        ))}
-      </div> */}
     </PageWrapper>
   );
-
-  // const trendingFilms = await fetchMovie('avatar');
 }

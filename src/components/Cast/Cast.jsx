94258671f00da1186af21cbd272de9149eb8b75e
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-16 22:01:28 +0200

    Feature/ add Cast logic + markup

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,3 +1,38 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { fetchMovieCast } from 'services/themoviedbApi';
+
 export default function Cast() {
-  return <div>Cast subPage</div>;
+  const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
+
+  useEffect(() => {
+    async function getMovieCast() {
+      try {
+        const movieCast = await fetchMovieCast(movieId);
+        setCast(movieCast);
+      } catch (error) {
+        console.log('error :>> ', error);
+      }
+    }
+    getMovieCast();
+  }, [movieId]);
+
+  return (
+    <div>
+      <ul>
+        {cast.map(({ id, profile_path, name, character }) => (
+          <li key={id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w300${profile_path}`}
+              alt={name}
+            />
+            <h3>{name}</h3>
+            <p>Character:</p>
+            <p>{character}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }

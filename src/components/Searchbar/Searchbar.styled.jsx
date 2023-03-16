1427e9f4991ef26efc0abfcffad960389543e89f
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-17 00:12:52 +0200

    Create Searchbar.styled.jsx

---
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import { Form, Field } from 'formik';
+
+export const SearchForm = styled(Form)`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  background-color: #fff;
+  border: 1px solid #d1d1d1;
+  border-radius: 5px;
+  overflow: hidden;
+`;
+
+export const SearchButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 36px;
+  height: 36px;
+  border: 0;
+  background-size: 40%;
+  background-repeat: no-repeat;
+  background-position: center;
+  opacity: 0.6;
+  transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  cursor: pointer;
+  outline: none;
+
+  font-size: 28px;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const SearchInput = styled(Field)`
+  display: inline-block;
+  width: 100%;
+  font: inherit;
+  font-size: 18px;
+  line-height: 1.5em;
+  border: none;
+  outline: none;
+  padding: 0 16px;
+
+  &::placeholder {
+    font: inherit;
+    font-size: 18px;
+  }
+`;

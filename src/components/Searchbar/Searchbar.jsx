be13b3b4c02db871632731a1670c3719c86091ea
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-17 00:12:46 +0200

    Create Searchbar.jsx

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Formik } from 'formik';
+import { SearchForm, SearchButton, SearchInput } from './Searchbar.styled';
+import { BiSearchAlt } from 'react-icons/bi';
+
+export function Searchbar({ handleSearchInput }) {
+  return (
+    <div>
+      <Formik
+        initialValues={{ input: '' }}
+        onSubmit={({ input }, { resetForm }) => {
+          if (input.trim() === '') return;
+          handleSearchInput(input);
+          resetForm();
+        }}
+      >
+        <SearchForm>
+          <SearchInput
+            type="text"
+            name="input"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search movies"
+          ></SearchInput>{' '}
+          <SearchButton type="submit">
+            <BiSearchAlt />
+          </SearchButton>
+        </SearchForm>
+      </Formik>
+    </div>
+  );
+}
+
+Searchbar.propTypes = {
+  handleSearchInput: PropTypes.func.isRequired,
+};

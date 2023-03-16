ad73b3ad02a38181947ef2cc5b4b30e3f794a28b
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-16 22:02:05 +0200

    Feature/ add Reviews logik and markup

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,3 +1,37 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { fetchMovieReviews } from 'services/themoviedbApi';
+
 export default function Reviews() {
-  return <div>Reviews subPage</div>;
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    async function getMovieReviews() {
+      try {
+        const movieCast = await fetchMovieReviews(movieId);
+        setReviews(movieCast);
+      } catch (error) {
+        console.log('error :>> ', error);
+      }
+    }
+    getMovieReviews();
+  }, [movieId]);
+
+  return (
+    <div>
+      {!reviews.length ? (
+        <p>We don`t have any reviews for this movie</p>
+      ) : (
+        <ul>
+          {reviews.map(({ id, author, content }) => (
+            <li key={id}>
+              <h3>Author: {author}</h3>
+              <p>{content}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
